c73acc99331d639629e1294f3a8617e117b89808
yangyuchun <yangyuchun@meshcloud.com>
2022-12-27 17:37:57 +0800

    feat: 打包文件

--- script/genNewComp/.template/docs/demo.vue.tpl
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <y-{{ compClassName }} />
-  </div>
-</template>
---
+++ src/App.vue.d.ts
@@ -0,0 +1,22 @@
+export default _sfc_main;
+declare namespace _sfc_main {
+    function setup(__props: any, { expose }: {
+        expose: any;
+    }): {
+        data: {
+            links: any;
+        };
+        ComponentList: any;
+        reactive: typeof reactive;
+    };
+    function setup(__props: any, { expose }: {
+        expose: any;
+    }): {
+        data: {
+            links: any;
+        };
+        ComponentList: any;
+        reactive: typeof reactive;
+    };
+}
+import { reactive } from "@vue/runtime-core";
